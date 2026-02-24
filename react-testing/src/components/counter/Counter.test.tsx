9ff71ce0978610f0b93eb9d79be7c70106c6b5e8
Pratik Tarkasband <tpratik345@gmail.com>
2026-02-24 12:18:16 +0530

    Added keyboard interaction tests

--- react-testing/src/components/counter/Counter.test.tsx
+++ react-testing/src/components/counter/Counter.test.tsx
@@ -40,4 +40,54 @@ describe('Counter', () => {
         const countElement = screen.getByRole('heading');
         expect(countElement).toHaveTextContent('2');
     })
+
+    test('set button is present', () => {
+        render(<Counter />);
+        const buttonElement = screen.getByRole('button', {
+            name: 'Set'
+        })
+        expect(buttonElement).toBeInTheDocument();
+    })
+
+    test('render count of 10 after clicking set button', async () => {
+        user.setup();
+        render(<Counter />);
+        const inputElement = screen.getByRole('spinbutton');
+        await user.type(inputElement, '10');
+        expect(inputElement).toHaveValue(10);
+        const buttonElement = screen.getByRole('button', {
+            name: 'Set'
+        })
+        await user.click(buttonElement);
+        const countElement = screen.getByRole('heading');
+        expect(countElement).toHaveTextContent('10');
+    })
+
+    test('elements are focused in right order', async () => {
+        user.setup();
+        render(<Counter />);
+        const incrementElement = screen.getByRole('button', {
+            name: 'Increment'
+        })
+        const setElement = screen.getByRole('button', {
+            name: 'Set'
+        })
+        const inputElement = screen.getByRole('spinbutton');
+        await user.tab();
+        expect(incrementElement).toHaveFocus();
+        await user.tab();
+        expect(inputElement).toHaveFocus();
+        await user.tab();
+        expect(setElement).toHaveFocus();
+    })
+
+    test('clear the input element', async () => {
+        user.setup();
+        render(<Counter />);
+        const inputElement = screen.getByRole('spinbutton');
+        await user.type(inputElement, '10');
+        expect(inputElement).toHaveValue(10);
+        await user.clear(inputElement);
+        expect(inputElement).toHaveValue(null);
+    })
 });
